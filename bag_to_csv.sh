32bdcb1835a4d8558d5aff7697d6f3aa913bdae6
Jarvis Schultz <schultzjarvis@gmail.com>
2015-02-20 15:12:24 -0600

    Just modified this script to dump optimization data CSV files. I then ran it on all data with the following commands:     $ cd PATH_TO_THIS_REPO 	$ find data/ -name "*.bag" -exec ./bag_to_csv.sh {} \;

--- bag_to_csv.sh
+++ bag_to_csv.sh
@@ -10,17 +10,19 @@ fi
 
 if [ -a ${filename} ] 
 then 
-    info=`rosbag info ${filename}`
-    meas=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'meas_config'`
-    filt=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'filt_config'`
-    ref=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'ref_config'`
-    ser=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'serial_commands'`
-    filtstate=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'filt_state'`
-    rostopic echo -p -b ${filename} $filt > ${filename%.*}_filt.txt 
-    rostopic echo -p -b ${filename} $meas > ${filename%.*}_meas.txt 
-    rostopic echo -p -b ${filename} $ref > ${filename%.*}_ref.txt 
-    rostopic echo -p -b ${filename} $ser > ${filename%.*}_ser.txt 
-    rostopic echo -p -b ${filename} $filtstate > ${filename%.*}_filtstate.txt 
+    # info=`rosbag info ${filename}`
+    # meas=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'meas_config'`
+    # filt=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'filt_config'`
+    # ref=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'ref_config'`
+    # ser=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'serial_commands'`
+    # filtstate=`echo $info | grep -e '\ \/[^ ]*' -o |grep -e 'filt_state'`
+    # rostopic echo -p -b ${filename} $filt > ${filename%.*}_filt.txt 
+    # rostopic echo -p -b ${filename} $meas > ${filename%.*}_meas.txt 
+    # rostopic echo -p -b ${filename} $ref > ${filename%.*}_ref.txt 
+    # rostopic echo -p -b ${filename} $ser > ${filename%.*}_ser.txt 
+    # rostopic echo -p -b ${filename} $filtstate > ${filename%.*}_filtstate.txt
+	rostopic echo -p -b ${filename} /robot_1/optimization_data > ${filename%.*}_opt_data.csv
+	rostopic echo -p -b ${filename} /robot_1/sim_optimization_data > ${filename%.*}_sim_opt_data.csv
 else
     echo "[ERROR] bag file not found: ${filename}"
 fi
